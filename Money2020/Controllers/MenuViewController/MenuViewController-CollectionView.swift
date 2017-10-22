a5160cc51ad5bce3baa2770e18eaea2fc2ec305f
Akkshay Khoslaa <akhoslaa@berkeley.edu>
2017-10-22 02:47:34 -0700

    cleaned up

---
+++ Money2020/Controllers/MenuViewController/MenuViewController-CollectionView.swift
@@ -0,0 +1,111 @@
+//
+//  MenuItemCollectionView.swift
+//  Money2020
+//
+//  Created by Akkshay Khoslaa on 10/22/17.
+//  Copyright © 2017 Akkshay Khoslaa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import PromiseKit
+
+extension MenuViewController {
+    
+    func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: headerView.frame.maxY, width: view.frame.width, height: view.frame.height - headerView.frame.maxY), collectionViewLayout: layout)
+        collectionView.register(MenuItemCollectionViewCell.self, forCellWithReuseIdentifier: "menuItemCell")
+        collectionView.register(MenuHeaderCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "header")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .clear
+        view.addSubview(collectionView)
+    }
+    
+}
+
+// MARK: CollectionView Delegate Methods
+
+extension MenuViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return filteredCategoryToMenuItem.keys.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filteredCategoryToMenuItem[Array(filteredCategoryToMenuItem.keys)[section]]!.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "menuItemCell", for: indexPath) as! MenuItemCollectionViewCell
+        
+        for subview in cell.contentView.subviews {
+            subview.removeFromSuperview()
+        }
+        
+        cell.awakeFromNib()
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        let cell = cell as! MenuItemCollectionViewCell
+        let item = filteredCategoryToMenuItem[Array(filteredCategoryToMenuItem.keys)[indexPath.section]]?[indexPath.item]
+        cell.nameLabel.text = item?.title!
+        cell.descriptionLabel.text = item?.description!
+        cell.priceLabel.text = InstantUtils.doubleToCurrencyString(val: (item?.price)!)
+        
+        firstly {
+            return item!.getImage()
+            }.then { img -> Void in
+                cell.foodImageView.image = img
+            }.catch(policy: .allErrors) { error in
+                log.error(error.localizedDescription)
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 130)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        switch kind {
+            
+        case UICollectionElementKindSectionHeader:
+            
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath) as! MenuHeaderCollectionReusableView
+            for subview in headerView.subviews {
+                subview.removeFromSuperview()
+            }
+            headerView.awakeFromNib()
+            headerView.titleLabel.text = Array(filteredCategoryToMenuItem.keys)[indexPath.section]
+            
+            return headerView
+            
+        default:
+            return UICollectionReusableView()
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 70)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let item = filteredCategoryToMenuItem[Array(filteredCategoryToMenuItem.keys)[indexPath.section]]?[indexPath.item]
+        showItemDetailView(forItem: item!)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let numSections = filteredCategoryToMenuItem.keys.count
+        if section == numSections - 1 {
+            return UIEdgeInsets(top: 0, left: 0, bottom: 30, right: 0)
+        }
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+}
---
+++ Money2020/Controllers/MenuViewController/MenuViewController-Search.swift
@@ -0,0 +1,117 @@
+//
+//  MenuItemSearchBar.swift
+//  Money2020
+//
+//  Created by Akkshay Khoslaa on 10/22/17.
+//  Copyright © 2017 Akkshay Khoslaa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import PromiseKit
+
+extension MenuViewController {
+    
+    func setupSearchBar() {
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        let navBarHeight = navigationController?.navigationBar.frame.height
+        headerView = UIView(frame: CGRect(x: -1, y: statusBarHeight + navBarHeight! + view.frame.height/2, width: view.frame.width + 2, height: 45))
+        headerView.layer.borderColor = UIColor(hex: "#EEEFF0").cgColor
+        headerView.layer.borderWidth = 0.8
+        headerView.backgroundColor = .white
+        searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 45))
+        searchBar.placeholder = " Search menu..."
+        searchBar.barStyle = .blackOpaque
+        searchBar.searchBarStyle = .minimal
+        searchBar.backgroundColor = .white
+        searchBar.barTintColor = .white
+        searchBar.layer.borderWidth = 0
+        searchBar.layer.cornerRadius = 3
+        searchBar.clipsToBounds = true
+        searchBar.delegate = self
+        UIBarButtonItem.appearance(whenContainedInInstancesOf: [UISearchBar.self]).setTitleTextAttributes([NSAttributedStringKey.font: UIFont(name: "SFUIText-Regular", size: 13)!], for: .normal)
+        
+        let searchField = searchBar.value(forKey: "searchField") as? UITextField
+        searchField?.font = UIFont(name: "SFUIText-Regular", size: 12)
+        searchField?.backgroundColor = .white
+        searchField?.textColor = UIColor(hex: "#9299A0")
+        searchField?.borderStyle = .none
+        headerView.addSubview(searchBar)
+        view.addSubview(headerView)
+    }
+    
+    @objc func searchMenu() {
+        filteredCategoryToMenuItem = [String: [MenuItem]]()
+        if searchWord == "" || searchWord == nil {
+            filteredCategoryToMenuItem = categoryToMenuItem
+        } else {
+            for (_, items) in categoryToMenuItem {
+                for item in items {
+                    if (item.category?.lowercased().contains((searchWord?.lowercased())!))! || (item.title?.lowercased().contains((searchWord?.lowercased())!))! || (item.description?.lowercased().contains((searchWord?.lowercased())!))! {
+                        if var catItems = filteredCategoryToMenuItem[item.category!] {
+                            catItems.append(item)
+                            filteredCategoryToMenuItem[item.category!] = catItems
+                        } else {
+                            filteredCategoryToMenuItem[item.category!] = [item]
+                        }
+                    }
+                }
+            }
+        }
+        collectionView.reloadData()
+    }
+    
+}
+
+//MARK: Search Delegate Methods
+
+extension MenuViewController: UISearchBarDelegate, UITextFieldDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.becomeFirstResponder()
+        searchMenu()
+        searchBar.showsCancelButton = true
+    }
+    
+    func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchWord = searchBar.text!
+        
+        if let timer = debounceTimer {
+            timer.invalidate()
+        }
+        
+        debounceTimer = Timer(timeInterval: 0.1, target: self, selector: #selector(searchMenu), userInfo: nil, repeats: false)
+        RunLoop.current.add(debounceTimer!, forMode: .defaultRunLoopMode as RunLoopMode)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+        searchBar.setShowsCancelButton(false, animated: true)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.showsCancelButton = false
+        searchBar.endEditing(true)
+        filteredCategoryToMenuItem = categoryToMenuItem
+        collectionView.reloadData()
+    }
+    
+}
